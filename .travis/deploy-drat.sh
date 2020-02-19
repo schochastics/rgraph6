1a7e0864e00c1d8b1ec8292aa1cc440f6c9ee2b7
Michal Bojanowski <michal2992@gmail.com>
2020-02-19 12:35:37 +0100

    Deploy on Windows with after_success
    Common script for drat deploys. Accepts path to package archive (source
    or binary) as an argument.

--- .travis/deploy-drat.sh
+++ .travis/deploy-drat.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
+# Usage: deploy-drat.sh [pkg.tarball | pkg.zip | pkg.tgz]
+
 set -o errexit -o verbose -o nounset
 
+PKG=$1
 PKG_REPO=$PWD
 DRAT=../drat
 
@@ -19,10 +22,11 @@ git -C $DRAT fetch upstream 2>err.txt
 git -C $DRAT checkout gh-pages
 
 echo "PWD=$PWD"
+echo "PKG=$PKG"
 echo "PKG_NAME=$PKG_NAME"
 echo "PKG_VERSION=$PKG_VERSION"
-echo "PKG_ZIP=$PKG_NAME"
+echo "PKG_ZIP=$PKG_ZIP"
 
-Rscript.exe -e "drat::insertPackage('$PKG_ZIP', repodir = '$DRAT', commit='Travis update: build $TRAVIS_BUILD_NUMBER of $PKG_NAME on Windows')"
+Rscript.exe -e "drat::insertPackage('$PKG', repodir = '$DRAT', commit='Travis update: build $TRAVIS_BUILD_NUMBER of $PKG_NAME: $PKG')"
 git -C $DRAT push 2>err.txt
 
